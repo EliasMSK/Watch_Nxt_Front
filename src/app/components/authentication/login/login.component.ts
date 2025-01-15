27ed3a7ae8c057b668943dfbabfcb3fd8c3d9fb6
camiladlc18 <camiladlc95@gmail.com>
2025-01-14 22:38:28 -0500

    Estilos Login

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../servicio/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [FormsModule],
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  loginData = { username: '', password: '' };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        console.log('Inicio de sesión exitoso:', response);
+        localStorage.setItem('access_token', response.access_token);
+        this.router.navigate(['/register']);
+
+      },
+      error: (error) => {
+        console.error('Error en el inicio de sesión:', error);
+      },
+    });
+  }
 }
--- src/app/servicio/auth.service.ts
+++ src/app/servicio/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = 'http://127.0.0.1:8000/auth'; 
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  register(data: { username: string; password: string; email: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register/`, data);
+  }
+
+  login(data: { username: string; password: string }): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login/`, data);
+  }
 }
